4e30397fb1905c2e967e0ad5a20d22c92e2a2a4c
Sam-a-n <samanma2214@gmail.com>
2022-12-20 03:54:20 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints haslf of a sting
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+	int l = 0;
+
+	while (*str != '\0')
+	{
+		l++;
+		str++;
+	}
+
+	str -= (l / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
